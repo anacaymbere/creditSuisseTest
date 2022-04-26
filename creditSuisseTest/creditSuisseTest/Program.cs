9a9d6728c426face8b9bd3fe2d9606fa06a544e5
Ana Carolina Aymber <anacaymbere@gmail.com>
2022-04-26 13:37:45 -0300

    rules interation

--- creditSuisseTest/creditSuisseTest/Program.cs
+++ creditSuisseTest/creditSuisseTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using creditSuisseTest.Data.Classes;
 using creditSuisseTest.Data.Interfaces;
 
@@ -11,20 +12,39 @@ namespace creditSuisseTest
             try
             {
 
-            Trade trade = new Trade();
+                Trade trade = new Trade();
 
-            Console.WriteLine("Input NextPaymentDate:");
-            trade.NextPaymentDate = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Input the date reference:");
+                DateTime dateRef = Convert.ToDateTime(Console.ReadLine());
 
-            Console.WriteLine("Input Value:");
-            trade.Value = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Input the number of trades:");
+                int qntTrades = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input ClientSector:");
-            trade.ClientSector = Console.ReadLine();
+                List<string> portifolio = new List<string>();
 
-            TradeTransaction tradeTransaction = new TradeTransaction();
+                for (int qntTradesProces = 0; qntTradesProces < qntTrades; qntTradesProces ++)
+                 {
+                    Console.WriteLine("Input NextPaymentDate:");
+                    trade.NextPaymentDate = Convert.ToDateTime(Console.ReadLine());
 
-            Console.WriteLine(tradeTransaction.GetCategory(trade));
+                    Console.WriteLine("Input Value:");
+                    trade.Value = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Input ClientSector:");
+                    trade.ClientSector = Console.ReadLine();
+
+                    TradeTransaction tradeTransaction = new TradeTransaction();
+
+                    portifolio.Add(tradeTransaction.GetCategory(trade));
+                 }
+
+                Console.WriteLine("-----------------");
+
+                foreach (var item in portifolio)
+                {
+                    
+                    Console.WriteLine(item);
+                }
 
             }
 
